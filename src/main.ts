aeda7baf96acc2b1321b4a9c9e068ea3ff68aa39
iufb <misakilover228@gmail.com>
2023-05-31 18:02:11 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: 'http://localhost:3001', credentials: true });
   await app.listen(3000);
 }
 bootstrap();
